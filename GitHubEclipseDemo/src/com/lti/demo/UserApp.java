13b419e435c53514506538ba034a9c772f2634c1
AkashBhagvant <akashbhagwant24@gmail.com>
2022-11-14 12:43:01 +0530

    First Commit

---
+++ GitHubEclipseDemo/src/com/lti/demo/UserApp.java
@@ -0,0 +1,12 @@
+package com.lti.demo;
+
+public class UserApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Employee e = new Employee(101,"jay",5000);
+		System.out.println(e);
+
+	}
+
+}
